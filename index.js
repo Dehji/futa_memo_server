39a0fc4f3e3c73d8d4023260fde56fc8f6e523ab
Michael <gadreelazazel@gmail.com>
2023-02-18 14:34:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const authRoutes = require("./routes/auth.js");
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 3030;
 
 require('dotenv').config();
 
